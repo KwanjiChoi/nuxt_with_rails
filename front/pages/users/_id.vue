a84acd4f87fc8228d36053286ea84ea1857cc129
KwanjiChoi <kwanji0921@gmail.com>
2021-03-15 03:49:04 +0900

    first commit

---
+++ front/pages/users/_id.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1> Hello, {{ name }} 元気ですか？</h1>
+</template>
+<script>
+export default {
+  asyncData({ $axios, params }) {
+    return $axios.$get(`${process.env.BASE_URL}users/${params.id}`)
+      .then((res) => {
+        return { name: res.name }
+      })
+  }
+}
+</script>
