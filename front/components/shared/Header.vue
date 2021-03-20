037fd96a81022790f7e0e7fb1755d94ab2e0512a
KwanjiChoi <kwanji0921@gmail.com>
2021-03-21 02:50:29 +0900

    done set up

---
+++ front/components/shared/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar :flat="true" color='white' app fixed>
+    <v-toolbar-title>
+        Portfolio
+    </v-toolbar-title>
+  </v-app-bar>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  header {
+    border-bottom: solid 1px #ecf0f1;
+    clear: both;
+  }
+
+  .v-application .white {
+    border-color: #ecf0f1 !important;
+  }
+
+</style>
